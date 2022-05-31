2a9b3decdb607f86feccb6324854c775e31906a5
Hirukamunasinghe <munasinghehiruka@gmail.com>
2022-05-31 22:11:31 +0530

    Initial commit

--- main.py
+++ main.py
@@ -10,13 +10,19 @@ def speed_calc_decorator(function):
         function()
         after_time = time.time()
         print(f"{function.__name__} run speed: {after_time - before_time}")
+    return wrapper_function
 
 
+@speed_calc_decorator
 def fast_function():
     for i in range(10000000):
         i * i
 
 
+@speed_calc_decorator
 def slow_function():
     for i in range(100000000):
         i * i
+
+fast_function()
+slow_function()
